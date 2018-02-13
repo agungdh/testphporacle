5cceefc82f0f5cb689776b22d942e634d6eb8cc0
agungdh <agungdh@live.com>
2018-02-13 10:29:48 +0700

    tambah contoh buat objek

---
+++ phpobject/1.php
@@ -0,0 +1,9 @@
+<?php
+$genericObject = new stdClass();
+$genericObject->nama = "AgungDH";
+$genericObject->kelas = "MI6A";
+$genericObject->npm = "15753003";
+// var_dump($genericObject);
+
+echo(json_encode($genericObject));
+?> 
